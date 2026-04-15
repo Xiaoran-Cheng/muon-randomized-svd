0e2e525e4de5ac489c316c0ae86c20741de30977
kakarottocheng <70917035+kakarottocheng@users.noreply.github.com>
2026-04-15 14:02:19 -0400

    best hyperparam

--- muon-randomized-svd/cifar10/cifar10.sh
+++ muon-randomized-svd/cifar10/cifar10.sh
@@ -6,15 +6,15 @@ source /home/xcheng328/miniconda3/bin/activate muon
 
 python airbench94_muon.py \
   --optimizer-mode muon \
-  --epochs 1 \
-  --num-trials 1 \
-  --val-every-steps 100 \
-  --wandb False \
-  --batch-size 2000 \
+  --epochs 8 \
+  --num-trials 50 \
+  --val-every-steps 10 \
+  --wandb True \
+  --batch-size 1000 \
   --sgd-momentum 0.85 \
   --sgd-nesterov True \
-  --muon-lr 0.24 \
-  --muon-momentum 0.6 \
+  --muon-lr 0.19572308843587835 \
+  --muon-momentum 0.48356183431214944 \
   --muon-nesterov True \
   --filter-sgd-lr 0.24 \
   --filter-sgd-weight-decay 0.0 \
@@ -23,10 +23,10 @@ python airbench94_muon.py \
   --adamw-eps 1e-8 \
   --filter-adamw-lr 0.24 \
   --filter-adamw-weight-decay 0.0 \
-  --inexact_solver quintic_ns_empirical \
-  --orth-steps 3 \
-  --randomized False \
-  --rank 32 \
-  --oversampling 2 \
-  --power-iters 0 \
+  --inexact_solver cubic_ns_theoretical \
+  --orth-steps 9 \
+  --randomized True \
+  --rank 256 \
+  --oversampling 10 \
+  --power-iters 2 \
   "$@"
--- muon-randomized-svd/cifar10/sweep.cmd
+++ muon-randomized-svd/cifar10/sweep.cmd
@@ -3,10 +3,10 @@ getenv     = true
 executable = sweep.sh
 
 # Usage: first create the sweep on the login node:
-#   wandb sweep sweep_e2_rank.yaml
+#   wandb sweep sweep.yaml
 # then paste the sweep ID below and submit:
 #   condor_submit sweep.cmd
-arguments  = xjc5161-penn-state/uncategorized/7d3lq1jh 50
+arguments  = xjc5161-penn-state/uncategorized/nnn9t5sv 100
 
 log    = /home/xcheng328/cifar10/$(Cluster).$(Process).log
 output = /home/xcheng328/cifar10/$(Cluster).$(Process).out
@@ -19,4 +19,4 @@ request_memory = 30GB
 requirements = (Machine == "isye-hpc0456.isye.gatech.edu") || \
                (Machine == "isye-hpc0458.isye.gatech.edu")
 
-queue 4
+queue 6
